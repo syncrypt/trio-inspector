c2912bcb559194c26721cd50bd029e98f7abec9c
Hannes Gräuler <hannes@smasi.de>
2019-03-15 08:50:51 +0100

    More involved, dynamic example

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,23 @@
 import trio
+from random import randint
 from trio_inspector import TrioInspector
+
+
+async def sleep_seconds(seconds):
+    await trio.sleep(seconds)
+
+
+async def stubborn_spawner(n):
+    while True:
+        async with trio.open_nursery() as nursery:
+            for i in range(n):
+                nursery.start_soon(sleep_seconds, randint(1,10))
 
 
 async def child2():
     async with trio.open_nursery() as nursery:
         nursery.start_soon(trio.sleep_forever)
-        nursery.start_soon(trio.sleep_forever)
+        nursery.start_soon(stubborn_spawner, 4)
 
 
 async def child1():
